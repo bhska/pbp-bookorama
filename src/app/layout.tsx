ea341b5226c9990fbf3d522b1a71453c8a0e89d2
Bhaska <muhammadbhaska0@gmail.com>
2025-12-03 11:04:40 +0700

    feat: added pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,29 @@
 import './globals.css';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import Providers from './providers';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const viewport: Viewport = {
+  themeColor: 'black',
+}
 export const metadata: Metadata = {
   title: 'Bookorama',
   description: 'Bookorama - Toko buku online',
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Bookorama',
+  },
 };
 
 export default function RootLayout({
